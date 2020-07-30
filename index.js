d066fd1652b5c3ebe12bd7064729a544c5e0e37e
demi-werkzam <d.werkzam@gmail.com>
2020-07-30 21:53:38 +0200

    user router added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { PORT } = require("./config/constants");
 const authRouter = require("./routers/auth");
 const restaurantRouter = require("./routers/restaurants");
 const likeRouter = require("./routers/likes");
+const userRouter = require("./routers/user");
 const authMiddleWare = require("./auth/middleware");
 
 const app = express();
@@ -155,6 +156,7 @@ app.post("/authorized_post_request", authMiddleWare, (req, res) => {
 app.use("/", authRouter);
 app.use("/restaurants", restaurantRouter);
 app.use("/likes", likeRouter);
+app.use("/users", userRouter);
 
 // Listen for connections on specified port (default is port 4000)
 
