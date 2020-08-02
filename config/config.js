d4cbb790f13016a9608f7c7f4b0879e1bc9d8c46
demi-werkzam <d.werkzam@gmail.com>
2020-08-02 21:29:14 +0200

    Added DATABASE_URL

--- config/config.js
+++ config/config.js
@@ -13,10 +13,8 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    url: process.env.DATABASE_URL,
+    dialect: "postgres",
+    operatorsAliases: "0",
   },
 };
